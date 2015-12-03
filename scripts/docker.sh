07bb0df2365c44a028413d526adab62e0f554929
Balazs Kutil <balazs@jut.io>
2015-12-03 19:25:01 +0100

    Wait for influx being up

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 NAME="influxdb"
 VER="1"
@@ -13,7 +13,16 @@ case "$CMD" in
 
         docker pull "$CONTAINER"
         docker run -d --name "$NAME" -p 8083:8083 -p 8086:8086 "$CONTAINER"
+
+        CURL=$(curl -s -X GET 'http://127.0.0.1:8086/query')
+
+        while [ "$?" -ne 0 ]; do
+            CURL=$(curl -s -X GET 'http://127.0.0.1:8086/query')
+            echo -n '.'
+            sleep 1
+        done
         ;;
+
     "stop")
         docker stop "$NAME"
         docker rm "$NAME"
